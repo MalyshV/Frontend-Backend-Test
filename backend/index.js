fcb7dca5487c60b0ec31e8f7e8dd3ccd9bd948ff
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-11-03 09:34:45 +0300

    feat: add .env

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,9 @@ const app = express();
 app.listen(PORT, () => {
     console.log(`СЕРВЕР ЗАПУСТИЛСЯ НА ПОРТУ 3001 ${PORT}`);
 })
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Hello from Backend"
+    })
+})
